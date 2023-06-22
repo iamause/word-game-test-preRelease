0f8c4605bfd640173358b31be0fc7f314c353b46
usama <112273462+iamause@users.noreply.github.com>
2023-06-22 20:37:00 +0500

    Add files via upload

---
+++ words.js
@@ -0,0 +1,203 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "high-level programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument with strings"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention or a goal"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "amazon",
+        hint: "online shopping site"
+    },
+    {
+        word: "javascript",
+        hint: "programming language"
+    },
+    {
+        word: "coding",
+        hint: "synonym of programming"
+    },
+    {
+        word: "iron man",
+        hint: "a legend of marvel universe"
+    },
+    {
+        word: "bugs",
+        hint: "enemy of programmers"
+    },
+    {
+        word: "spiderman",
+        hint: "a famous red super hero"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+    {
+        word: "mental",
+        hint: "related to the mind"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "related to a indoor game also a board game"
+    },
+    {
+        word: "tiktok",
+        hint: "a social media app"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "cpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "liberty",
+        hint: "a famous statue in new york"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "piano",
+        hint: "a musical instrument"
+    },
+    {
+        word: "nepal",
+        hint: "developing country name"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "an electric car company"
+    },
+    {
+        word: "mars",
+        hint: "red planet of our solar system"
+    },
+    {
+        word: "father",
+        hint: "synonym of dad"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "related to a gas or a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "thanos",
+        hint: "A marvel villan"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal to unlock things"
+    },
+    {
+        word: "egypt",
+        hint: "a very old country's name"
+    },
+    {
+        word: "joker",
+        hint: "psychological thriller film"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+    {
+        word: "ketchup",
+        hint: "type of tomato sauce"
+    },
+
+]
